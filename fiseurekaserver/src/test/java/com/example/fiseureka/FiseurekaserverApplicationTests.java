d90d24d1f80352b3229d8c89244c3fad91e28068
praveen-rps <109707042+praveen-rps@users.noreply.github.com>
2022-08-18 12:25:43 +0530

    Add files via upload

---
+++ fiseurekaserver/src/test/java/com/example/fiseureka/FiseurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fiseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiseurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
