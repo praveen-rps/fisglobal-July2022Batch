7c3cd12c70e6d2e0ec26c87401e65d23800bf186
praveen-rps <109707042+praveen-rps@users.noreply.github.com>
2022-08-18 12:25:18 +0530

    Add files via upload

--- fiscommentservice/src/main/java/com/example/microservices/comments/CommentserviceApplication.java
+++ fiscommentservice/src/main/java/com/example/microservices/comments/CommentserviceApplication.java
@@ -2,11 +2,13 @@ package com.example.microservices.comments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.example.microservices.comments")
+@EnableEurekaClient
 public class CommentserviceApplication {
 
 	public static void main(String[] args) {
